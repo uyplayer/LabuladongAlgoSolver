fda20c88a41c022a50bcea886eb8b428fce4caa8
uyplayer <32848979@qq.com>
2024-08-16 11:38:14 +0800

    add deque

--- 极速入门-数据结构基础/双端队列/ArrayDeque.cpp
+++ 极速入门-数据结构基础/双端队列/ArrayDeque.cpp
@@ -1,3 +1,53 @@
 //
 // Created by uyplayer on 2024-08-14.
 //
+
+
+
+#include <iostream>
+#include <vector>
+
+
+template<typename T>
+class ArrayDe {
+    ArrayDe() = default;
+    explicit ArrayDe(size_t size):data(size){};
+    // 拷贝构造函数
+    ArrayDe(const ArrayDe& other):data(other.data){};
+    ArrayDe(ArrayDe&& other) noexcept : data(std::move(other.data)){};
+    ArrayDe& operator=(ArrayDe&& other) noexcept {
+        if (this != &other) {
+             data = std::move(other.data);
+        }
+         return *this;
+    }
+
+     ~ArrayDe() = default;
+
+    void push_back(const T& value) {
+        data.push_back(value);
+    }
+
+    T& operator[](size_t index) {
+        return data[index];
+    }
+
+    const T& operator[](size_t index) const {
+        return data[index];
+    }
+
+
+    size_t size() const {
+        return data.size();
+    }
+
+    void print() const {
+        for (const auto& item : data) {
+            std::cout << item << " ";
+        }
+        std::cout << std::endl;
+    }
+private:
+    std::vector<T> data;
+};
+
--- 极速入门-数据结构基础/双端队列/LinkedListDeque.cpp
+++ 极速入门-数据结构基础/双端队列/LinkedListDeque.cpp
@@ -1,3 +1,117 @@
 //
 // Created by uyplayer on 2024-08-14.
 //
+
+
+#include <iostream>
+#include <stdexcept>
+
+
+
+
+
+template<typename T>
+class LinkedListDeque {
+private:
+    struct Node {
+        T value;
+        Node* prev;
+        Node* next;
+        explicit Node(T val):value(val),prev(nullptr),next(nullptr){};
+
+    };
+
+    Node* head;
+    Node* tail;
+    size_t size;
+
+
+public:
+    LinkedListDeque() : head(nullptr), tail(nullptr), size(0) {};
+     ~LinkedListDeque() {
+        clear();
+    }
+    void addFirst(T value){
+         Node* newNode = new Node(value);
+        if (head == nullptr){
+            head = tail = newNode;
+        } else {
+             newNode->next = head;
+             head->prev = newNode;
+              head = newNode;
+        }
+         ++size;
+     }
+
+     void addLast(T value){
+         Node* newNode = new Node(value);
+          if (tail == nullptr) {  // Empty deque
+            head = tail = newNode;
+        } else {
+            newNode->prev = tail;
+            tail->next = newNode;
+            tail = newNode;
+        }
+        ++size;
+
+     }
+
+      T removeFirst() {
+        if (isEmpty()) {
+            throw std::out_of_range("Deque is empty");
+        }
+        Node* oldHead = head;
+        T value = head->value;
+        head = head->next;
+        if (head != nullptr) {
+            head->prev = nullptr;
+        } else {
+            tail = nullptr;
+        }
+        delete oldHead;
+        --size;
+        return value;
+    }
+    T removeLast() {
+        if (isEmpty()) {
+            throw std::out_of_range("Deque is empty");
+        }
+        Node* oldTail = tail;
+        T value = tail->value;
+        tail = tail->prev;
+        if (tail != nullptr) {
+            tail->next = nullptr;
+        } else {  // The deque is now empty
+            head = nullptr;
+        }
+        delete oldTail;
+        --size;
+        return value;
+    }
+
+    bool isEmpty() const {
+        return size == 0;
+    }
+
+    size_t getSize() const {
+        return size;
+    }
+
+
+    void clear() {
+        while (!isEmpty()) {
+            removeFirst();
+        }
+    }
+
+
+    void printDeque() const {
+        Node* current = head;
+        while (current != nullptr) {
+            std::cout << current->value << " ";
+            current = current->next;
+        }
+        std::cout << std::endl;
+    }
+
+};
