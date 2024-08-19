59d58c44c235301ed7c9a0c390a36574008e905c
uyplayer <32848979@qq.com>
2024-08-19 14:56:26 +0800

    add ChainingHashMap

--- 极速入门-数据结构基础/哈希表/hashtable-chaining.cpp
+++ 极速入门-数据结构基础/哈希表/hashtable-chaining.cpp
@@ -7,24 +7,71 @@
 #include <vector>
 #include <algorithm>
 
-template<typename Tk,typename Tv>
-class ChainingHashMap{
+template<typename Tk, typename Tv>
+class ChainingHashMap {
 
 private:
     struct KVNode {
-            Tk key;
-            Tv value;
-            KVNode(Tk key,Tv value):key(key),value(value){};
+        Tk key;
+        Tv value;
+
+        KVNode(Tk key, Tv value) : key(key), value(value) {};
     };
 
     std::vector<std::list<KVNode>> buckets;
     size_t bucket_count;
 
 
-    size_t  HashFuck(const Tk& key){
+    size_t HashFuck(const Tk &key) {
         std::hash<Tk> hasher;
         return hasher(key) % bucket_count;
     }
+
+
+public:
+    explicit ChainingHashMap(size_t bucket_count = 10) : bucket_count(bucket_count) {
+        buckets.resize(bucket_count);
+    }
+
+    void insert(const Tk &key, const Tv &value) {
+
+        size_t index = hashFunction(key);
+        // key 存在
+        for (auto &node: buckets[index]) {
+            if (node.key == key) {
+                node.value = value;
+                return;
+            }
+        }
+        // key 不存在
+        buckets[index].emplace_back(key, value);
+    }
+
+    Tv *find(const Tk &key) {
+        size_t index = hashFunction(key);
+        for (auto &node: buckets[index]) {
+            if (node.key == key) {
+                return &node.value;
+            }
+        }
+        return nullptr;
+    }
+    // 删除操作
+    bool erase(const Tk &key) {
+
+        size_t index = hashFunction(key);
+        // 先找key 对应的桶
+        auto &bucket = buckets[index];
+        // 遍历对应的node 和 key
+        for (auto it = bucket.begin(); it != bucket.end(); ++it) {
+            if (it->key == key) {
+                bucket.erase(it);
+                return true;
+            }
+        }
+        return false;
+    }
+
 
 
 };
