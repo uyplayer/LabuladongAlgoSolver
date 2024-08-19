a5aae5378495d2de4372bfe4259a64b7cc0629af
uyplayer <32848979@qq.com>
2024-08-19 10:42:48 +0800

    add 哈希表

--- 极速入门-数据结构基础/哈希表/hashtable-chaining.cpp
+++ 极速入门-数据结构基础/哈希表/hashtable-chaining.cpp
@@ -1,3 +1,21 @@
 //
 // Created by uyplayer on 2024-08-19.
 //
+
+#include <iostream>
+#include <list>
+
+
+template<typename Tk,typename Tv>
+class ChainingHashMap{
+
+private:
+    struct KVNode {
+            Tk key;
+            Tv value;
+            KVNode(Tk key,Tv value):key(key),value(value){};
+    };
+
+
+    std::list<KVNode> hashMap;
+};
