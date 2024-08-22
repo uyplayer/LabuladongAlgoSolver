f4ba686373270fc9f367fb2c06b6ab48159096b2
uyplayer <32848979@qq.com>
2024-08-22 11:12:44 +0800

    add ExampleLinearProbingHashMap

--- 极速入门-数据结构基础/哈希表/OpenAddressingHashMap.cpp
+++ 极速入门-数据结构基础/哈希表/OpenAddressingHashMap.cpp
@@ -62,12 +62,79 @@ public:
         while (table[index] != nullptr) {
             auto entry = table[index];
             table[index] = nullptr;
-            put(entry->key, entry->val);
+            put(entry->key, entry->value);
             index = (index + 1) % table.size();
 
         }
 
 
+    }
+
+
+};
+
+
+class ExampleLinearProbingHashMapSymbol {
+private:
+    struct Node {
+        int key;
+        int value;
+
+        Node(int key, int value) : key(key), value(value) {};
+    };
+
+    Node *DELETED = new Node{-2, -2};
+    std::vector<Node *> table;
+
+    int hash(int key) {
+        return key / table.size();
+    }
+
+    int findKeyIndex(int key) {
+
+        for (int i = hash(key), step = 0; table[i] != nullptr; i = (i + 1) % table.size()) {
+            if (table[i] == DELETED) continue;
+            if (table[i]->key == key) return i;
+            if (++step == table.size()) return -1;
+        }
+
+        return -1;
+
+    }
+public:
+     explicit ExampleLinearProbingHashMapSymbol(int initCapacity) {
+        table.resize(initCapacity, nullptr);
+    }
+
+    // 增/改
+    void put(int key, int val) {
+        int index = findKeyIndex(key);
+
+        if (index != -1 && table[index] != nullptr) {
+            table[index]->value = val;
+            return;
+        }
+
+        Node* node = new Node{key, val};
+        index = hash(key);
+        while (table[index] != nullptr && table[index] != DELETED) {
+            index = (index+1) % table.size();
+        }
+        table[index] = node;
+    }
+
+
+    void remove(int key) {
+        int index = findKeyIndex(key);
+
+        if (index == -1) return;
+        // 直接用占位符表示删除
+        table[index] = DELETED;
+    }
+
+    int get(int key) {
+        int index = findKeyIndex(key);
+        return (index != -1) ? table[index]->value : -1;
     }
 
 
